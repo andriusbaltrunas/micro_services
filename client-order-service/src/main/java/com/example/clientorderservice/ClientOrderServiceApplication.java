5ada00c0aca60e7ddb9331f0cfe8bef786362cdf
andriusbaltrunasme <andrius.baltrunas@smefinance.lt>
2022-04-04 16:37:55 +0300

    git init project commit

---
+++ client-order-service/src/main/java/com/example/clientorderservice/ClientOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientOrderServiceApplication.class, args);
+    }
+
+}
---
+++ client-order-service/src/test/java/com/example/clientorderservice/ClientOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
